c063e9db307f53887831f3ebd7d3f413c7c81475
priyankatrivedi315-design <priyankatrivedi315@gmail.com>
2026-05-06 07:48:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  function selectOption(option)
+   {
+    document.getElementById("result").innerHTML =
+      "You selected: <strong>" + option + "</strong> for travel in India.";
+  }
+
+</script>
